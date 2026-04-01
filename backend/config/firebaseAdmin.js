a39927251578a9844e8207f2822f3fdcf1c74135
haneenmuhammedkc <haneenmuhammed369@gmail.com>
2026-04-01 11:48:19 +0530

    remove firebase json key

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,14 +1,13 @@
 import admin from "firebase-admin"
-import fs from "fs"
-import path from "path"
-import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf8")
-)
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
